1401c3dc8630c0db0c8076999d8170e9b18c8ba2
peterd100 <146393096+peterd100@users.noreply.github.com>
2024-04-30 08:14:58 -0400

    Update utils.h

--- utils.h
+++ utils.h
@@ -1,9 +1,9 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-// Utility function to seed the random number generator
+// seed the random number generator
 void seedRandom(unsigned int seed);
-// Utility function to generate a random number within a range
+//  random number within a range
 int generateRandom(int lower, int upper);
 
 #endif // UTILS_H
